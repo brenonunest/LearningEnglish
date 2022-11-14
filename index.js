f7556acfa4b3489bd7608632b1a10fed95a3c427
Breno Nunes <brenodh@hotmail.com>
2022-11-14 14:25:49 -0300

    Server Created

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require ("express")
+const app = express ()
+
+
+app.listen(3000, ()=>{
+    console.log("Sucesso") // funciona para printar e mostrar o que esta acontecendo com determinada funçao
+})
